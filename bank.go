d71b1fe2ef5a1b193596d777a6ea3bbeab0d1c72
Spencer Whitehead <53270623+spwhitehead@users.noreply.github.com>
2024-03-15 12:14:03 -0600

    Adds options, starts if statements

--- bank.go
+++ bank.go
@@ -1,7 +1,32 @@
 package main
 
+import "fmt"
+
 func main(){
+	var accountBalance = 1000.00
 
 
-	
+	fmt.Println("Welcome to Go Bank!")
+	fmt.Println("What do you want to do?")
+	fmt.Println("1. Check Balance")
+	fmt.Println("2. Deposit Money")
+	fmt.Println("3. Withdraw Money")
+	fmt.Println("4. Exit")
+
+	// Get user input
+
+	var choice int
+	fmt.Print("Your choice: ")
+	fmt.Scan(&choice)
+
+	wantsCheckBalance := choice == 1
+	// wantsDepositMoney := choice == 2
+	// wantsWithdrawMoney := choice == 3
+	// wantsExit := choice == 4
+
+	if wantsCheckBalance {
+		fmt.Println("Your balance is:", accountBalance)
+	}
+
+	fmt.Println("Your choice:", choice)
 }
