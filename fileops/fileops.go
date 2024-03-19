48f69b45b2b843ec964f31dba3a778a5fa14ad92
Spencer Whitehead <53270623+spwhitehead@users.noreply.github.com>
2024-03-19 15:56:36 -0600

    Fixes spelling errors in fileops

--- fileops/fileops.go
+++ fileops/fileops.go
@@ -11,14 +11,14 @@ func GetFloatFromFile(fileName string) (float64, error) {
 	data, err := os.ReadFile(fileName)
 
 	if err != nil {
-		return 0, errors.New("Failed to find file")
+		return 0, errors.New("failed to find file")
 	}
 
 	valueText := string(data)
 	value, err := strconv.ParseFloat(valueText, 64)
 
 	if err != nil{
-		return 0, errors.New("Failed to parse stored value.")
+		return 0, errors.New("failed to parse stored value")
 	}
 
 	return value, nil
