072b81b56cef8065390af5708b2a6015a5a0ac2c
Spencer Whitehead <53270623+spwhitehead@users.noreply.github.com>
2024-03-19 15:41:03 -0600

    Implements new files and packages

--- bank.go
+++ bank.go
@@ -44,12 +44,7 @@ func main(){
 	fmt.Println("Welcome to Go Bank!")
 	for {
 		
-		fmt.Println()
-		fmt.Println("What do you want to do?")
-		fmt.Println("1. Check Balance")
-		fmt.Println("2. Deposit Money")
-		fmt.Println("3. Withdraw Money")
-		fmt.Println("4. Exit")
+		presentOptions()
 
 		// Get user input
 
